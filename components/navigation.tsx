d6d8179d5a968e0dbe16f3595f33ea73c44f22a5
Kellswork <kellogbonna@gmail.com>
2023-12-02 10:53:52 +0100

    add navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import BaseLayout from './baseLayout';
+import BaseLayout from "./baseLayout";
 import Head from "next/head";
+import Link from "next/link";
 
 const Navigation = () => {
   return (
     <div className="bg-slate-100">
       <BaseLayout>
-        <div>
-          <h1 className="font-pacifico">AgirlCodes</h1>
+        <div className="flex justify-between items-center h-20 ">
+          <div>
+            <h1 className="font-pacifico text-2xl">AgirlCodes</h1>
+          </div>
+          <div className="font-roboto font-semibold text-base">
+            <Link className='mr-5' href="/">Home</Link>
+            <Link className='ml-5' href="/">Portfolio</Link>
+          </div>
         </div>
       </BaseLayout>
     </div>
