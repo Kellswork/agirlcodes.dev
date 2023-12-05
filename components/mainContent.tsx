c81ab0348f30d05d90c48ffe5c6f533cffc98964
Kellswork <kellogbonna@gmail.com>
2023-12-05 14:21:17 +0100

    update main-content ui for card profile view

--- components/mainContent.tsx
+++ components/mainContent.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import BaseLayout from "./baseLayout";
 import Divider from "./molecules/divider";
-import { BlogPostlistCard, ProfileCard } from "./molecules/cards";
+import { BlogPostlistCard, ProfileCard, ProfileCardMobile } from "./molecules/cards";
 import { FilterChipUnselected } from "./molecules/button";
 
 const MainContent = () => {
   return (
     <BaseLayout>
-      <div className="flex mt-24 mb-8 justify-between">
-        <div>
+      <div className="main-content flex mt-14 mb-8 justify-between md:flex-col-reverse">
+        <div className="blog-post-list-section">
           <Divider />
-          <div className="codelang-filter-chip flex justify-between w-9/10 mt-3">
+          <div className="codelang-filter-chip flex justify-between w-9/10 mt-5">
             <FilterChipUnselected />
             <FilterChipUnselected />
             <FilterChipUnselected />
@@ -24,6 +24,7 @@ const MainContent = () => {
           </div>
         </div>
         <ProfileCard />
+        <ProfileCardMobile/>
       </div>
     </BaseLayout>
   );
