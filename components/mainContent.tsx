24314fad67526f58bb923a1f6530a8c058554d75
Kellswork <kellogbonna@gmail.com>
2023-12-05 10:41:25 +0100

    build main container ui

--- components/mainContent.tsx
+++ components/mainContent.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import BaseLayout from "./baseLayout";
+import Divider from "./molecules/divider";
+import { BlogPostlistCard, ProfileCard } from "./molecules/cards";
+import { FilterChipUnselected } from "./molecules/button";
 
 const MainContent = () => {
   return (
-    <div>mainContent</div>
-  )
-}
+    <BaseLayout>
+      <div className="flex mt-24 mb-8 justify-between">
+        <div>
+          <Divider />
+          <div className="codelang-filter-chip flex justify-between w-9/10 mt-3">
+            <FilterChipUnselected />
+            <FilterChipUnselected />
+            <FilterChipUnselected />
+            <FilterChipUnselected />
+            <FilterChipUnselected />
+          </div>
+          <div className="post-list">
+            <BlogPostlistCard />
+            <BlogPostlistCard />
+            <BlogPostlistCard />
+          </div>
+        </div>
+        <ProfileCard />
+      </div>
+    </BaseLayout>
+  );
+};
 
 export default MainContent;
