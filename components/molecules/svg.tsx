914d3d76abb23dc8d27063ba94523d56a8b5ff0e
Kellswork <kellogbonna@gmail.com>
2023-12-05 10:39:43 +0100

    add svgs, remember to shrink the svg size later

--- components/molecules/svg.tsx
+++ components/molecules/svg.tsx
@@ -15,3 +15,144 @@ export const Tick = () => (
     />
   </svg>
 );
+
+export const Mail = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z"
+        stroke="#51279B"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M22 6L12 13L2 6"
+        stroke="#51279B"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export const Globe = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+      stroke="#51279B"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M2 12H22"
+      stroke="#51279B"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M12 2C14.5013 4.73835 15.9228 8.29203 16 12C15.9228 15.708 14.5013 19.2616 12 22C9.49872 19.2616 8.07725 15.708 8 12C8.07725 8.29203 9.49872 4.73835 12 2Z"
+      stroke="#51279B"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+
+export const LinkedIn = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M13.3333 6.66663C14.6593 6.66663 15.9311 7.19341 16.8688 8.13109C17.8065 9.06877 18.3333 10.3405 18.3333 11.6666V17.5H14.9999V11.6666C14.9999 11.2246 14.8243 10.8007 14.5118 10.4881C14.1992 10.1756 13.7753 9.99996 13.3333 9.99996C12.8912 9.99996 12.4673 10.1756 12.1547 10.4881C11.8422 10.8007 11.6666 11.2246 11.6666 11.6666V17.5H8.33325V11.6666C8.33325 10.3405 8.86004 9.06877 9.79772 8.13109C10.7354 7.19341 12.0072 6.66663 13.3333 6.66663Z"
+      stroke="#51279B"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M5.00008 7.5H1.66675V17.5H5.00008V7.5Z"
+      stroke="#51279B"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M3.33341 4.99996C4.25389 4.99996 5.00008 4.25377 5.00008 3.33329C5.00008 2.41282 4.25389 1.66663 3.33341 1.66663C2.41294 1.66663 1.66675 2.41282 1.66675 3.33329C1.66675 4.25377 2.41294 4.99996 3.33341 4.99996Z"
+      stroke="#51279B"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+
+export const GitHub = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_236_119)">
+      <path
+        d="M7.50008 15.8334C3.33341 17.0834 3.33341 13.75 1.66675 13.3334M13.3334 18.3334V15.1084C13.3647 14.711 13.311 14.3115 13.1759 13.9365C13.0409 13.5615 12.8275 13.2195 12.5501 12.9334C15.1667 12.6417 17.9167 11.65 17.9167 7.10003C17.9165 5.93655 17.469 4.8177 16.6667 3.97503C17.0466 2.95712 17.0198 1.83199 16.5917 0.833363C16.5917 0.833363 15.6084 0.541696 13.3334 2.0667C11.4234 1.54905 9.41007 1.54905 7.50008 2.0667C5.22508 0.541696 4.24175 0.833363 4.24175 0.833363C3.81373 1.83199 3.78687 2.95712 4.16675 3.97503C3.35852 4.82395 2.91052 5.95292 2.91675 7.12503C2.91675 11.6417 5.66675 12.6334 8.28341 12.9584C8.00925 13.2417 7.7978 13.5795 7.66284 13.95C7.52788 14.3204 7.47242 14.7151 7.50008 15.1084V18.3334"
+        stroke="#51279B"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_236_119">
+        <rect width="20" height="20" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export const Gmail = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M3.33341 3.33337H16.6667C17.5834 3.33337 18.3334 4.08337 18.3334 5.00004V15C18.3334 15.9167 17.5834 16.6667 16.6667 16.6667H3.33341C2.41675 16.6667 1.66675 15.9167 1.66675 15V5.00004C1.66675 4.08337 2.41675 3.33337 3.33341 3.33337Z"
+      stroke="#51279B"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M18.3334 5L10.0001 10.8333L1.66675 5"
+      stroke="#51279B"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
