5e5b38f96d74dc93f01671ba825a8491f92ec49e
Kellswork <kellogbonna@gmail.com>
2023-12-06 09:38:03 +0100

    update config file with - screen breakpoints - colors - fontsize - i did use some on of values that i didnt repeat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
- 
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
     // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'xl': {'min': '998px'},
+      'lg': {'min': '998px', 'max': '1090px'},
+      'md': {'min': '668px', 'max': '997px'},
+      'sm' : {'max': '667px'},
+      'ft' :{ 'max': '820px'}
+    },
     extend: {
       fontFamily: {
-        'pacifico':['var(--font-pacifico)', 'cursive'],
-        'roboto': ['var(--font-roboto)', 'sans-serif', 'ui-sans-serif', 'system-ui',]
+        pacifico: ["var(--font-pacifico)", "cursive"],
+        roboto: [
+          "var(--font-roboto)",
+          "sans-serif",
+          "ui-sans-serif",
+          "system-ui",
+        ],
       },
       maxWidth: {
-        "5xl" : '1030px',
+        "5xl": "1030px",
       },
-     fontSize: {
-      titleLarge: ['1.375rem', {
-        lineHeight: '28px'
-      }]
-     },
+      fontSize: {
+        titleLarge: ["1.375rem", { lineHeight: "28px" }],
+        headlineMedium: ["1.75rem", { lineHeight: "26px" }],
+        headlineSmall: ["1.5rem", { lineHeight: "32px" }],
+      },
+      colors: {
+        'purple-10':'#240754',
+        'purple-9': '#34126F',
+        'purple-7' : '#51279B',
+        'purple-5': "#724BB7",
+        'purple-4': '#8662C7',
+        'purple-1': '#EAE2F8',
+        'text-color': '#1D192B',
+        'place-holder-color': '#49454F',
+        'neutral-100': '#f1f5f880'
+      }
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
