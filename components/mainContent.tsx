931bdc69e2905f95ff57f90db7346aa16cab3315
Kellswork <kellogbonna@gmail.com>
2023-12-07 13:18:24 +0100

    refractor-maincontnt page

--- components/mainContent.tsx
+++ components/mainContent.tsx
@@ -1,27 +1,19 @@
 import React from "react";
 import BaseLayout from "./baseLayout";
 import Divider from "./molecules/divider";
-import { BlogPostlistCard, ProfileCard, ProfileCardMobile } from "./molecules/cards";
-import { FilterChipSelected, FilterChipUnselected } from "./molecules/button";
+import { ProfileCard, ProfileCardMobile } from "./molecules/cards";
+import { PostDataProps } from "../utils/util";
+import BlogPostList from "./blogPostList";
 
-const MainContent = () => {
+
+const MainContent = ({allPostsData}: {allPostsData:PostDataProps[]}) => {
+  console.log('posts', allPostsData)
   return (
     <BaseLayout>
       <div className="main-content flex mt-14 mb-8 justify-between md:flex-col-reverse sm:flex-col-reverse">
         <div className="blog-post-list-section">
           <Divider />
-          <div className="codelang-filter-chip flex justify-between w-9/10 my-5 md:w-[90%] sm:flex-wrap">
-            <FilterChipSelected />
-            <FilterChipUnselected />
-            <FilterChipUnselected />
-            <FilterChipUnselected />
-            <FilterChipUnselected />
-          </div>
-          <div className="post-list">
-            <BlogPostlistCard />
-            <BlogPostlistCard />
-            <BlogPostlistCard />
-          </div>
+          <BlogPostList posts={allPostsData} />
         </div>
         <ProfileCard />
         <ProfileCardMobile/>
@@ -31,3 +23,4 @@ const MainContent = () => {
 };
 
 export default MainContent;
+
