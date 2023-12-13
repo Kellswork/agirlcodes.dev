200f3bf47e743b93c90ad094b987b586bd1caecc
Kellswork <kellogbonna@gmail.com>
2023-12-13 10:12:31 +0100

    add meta dat to layout and use in index.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,26 +1,52 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 interface LayoutProps {
-  children: JSX.Element | JSX.Element[]
-
+  children: JSX.Element | JSX.Element[];
+  className: string
 }
 
-export default function Layout({ children }: LayoutProps){
+const siteDetails ={
+  title: "agirlcodes.dev",
+  description: "Welcome to my software development blog! Here, I provide valuable insights into modern web development using various technologies such as React JS, Typescript, Express/Node JS, Golang, and PostgreSQL. If you're looking for comprehensive articles on these technologies, you've come to the right place.",
+  image: "/agirlcodes.jpg",
+}
+
+export default function Layout({ children, className }: LayoutProps) {
   return (
-    <div>
+    <div className={`${className}`} >
       <Head>
-       <link
+        <meta name="description" content={siteDetails.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={siteDetails.title} />
+        <meta property="og:description" content={siteDetails.description} />
+        <meta
+          property="og:image"
+          content={`https://www.agirlcodes.dev${siteDetails.image}`}
+        />
+        <meta property="og:url" content="https://www.agirlcodes.dev" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:description" content={siteDetails.description} />
+        <meta name="twitter:title" content={siteDetails.title} />
+        <meta name="twitter:site" content="https://www.agirlcodes.dev" />
+        <meta
+          name="twitter:image"
+          content={`https://www.agirlcodes.dev${siteDetails.image}`}
+        />
+        <meta name="twitter:creator" content="@kelly_perrie" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="https://www.agirlcodes.dev" />
+        <link
           rel="icon"
           type="image/png"
           sizes="96x96"
           href="/favicon-96x96.png"
-        />
+        ></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <title>{siteDetails.title}</title>
       </Head>
-      
       <main>{children}</main>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,18 @@ import { PostDataProps, getSortedPostsData } from '../utils/util';
 import Navigation from '../components/navigation';
 import MainContent from '../components/mainContent';
 import Footer from '../components/footer';
+import Layout from '../components/layout';
 
 interface IndexProp {
   allPostsData: PostDataProps[]
 }
 export default function Home({allPostsData}: IndexProp) {
   return (
-    <div className='border-t-8 border-purple-5'>
+    <Layout className='border-t-8 border-purple-5'>
       <Navigation/>
       <MainContent allPostsData = {allPostsData}/>
       <Footer/>
-    </div>
+    </Layout>
   );
 }
 
